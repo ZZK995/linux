8e95f513b34bb0ba777a64ff6664282e94a019ed
Zzk995 <983558459@qq.com>
2020-01-08 12:35:39 +0800

    add error handling function error_request()

--- websever.c
+++ websever.c
@@ -81,3 +81,21 @@ int is_static(char *uri)
 		return 0;
 }
 
+void error_request(int fd,char *cause,char *errnum,char *shortmsg,char *description)
+{
+	char buf[MAXLINE],body[MAXLINE];
+	sprintf(body,"<html><title>error request</title>");
+	sprintf(body,"%s<body bgcolor=""ffffff"">\r\n",body);
+	sprintf(body,"%s%s:%s\r\n",body,errnum,shortmsg);
+	sprintf(body,"%s<p>%s:%s\r\n",body,description,cause);
+	sprintf(body,"%s<hr><em> Web server</em>\r\n",body);
+
+	sprintf(buf,"HTTP/1.0 %s %s \r\n",errnum,shortmsg);
+	rio_writen(fd,buf,strlen(buf));
+	sprintf(buf,"Content-type:text/html\r\n");
+	rio_writen(fd,buf,strlen(buf));
+	sprintf(buf,"COntent-length:%d\r\n\r\n",(int)strlen(body));
+	rio_writen(fd,buf,strlen(buf));
+	rio_writen(fd,buf,strlen(body));
+}
+
