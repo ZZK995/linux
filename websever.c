1d4c14cbddd8dfb5f20be4ef8211d54815c1469a
Zzk995 <983558459@qq.com>
2020-01-08 09:08:19 +0800

    add some function declarations to webserver.h

--- websever.c
+++ websever.c
@@ -1,3 +1,16 @@
 #include "wrapper.h"
 
 void process_trans(int fd);
+void read_requesthdrs(rio_t *rp);
+int is_static(char *uri);
+void parse_static_uri(char *uri,char *filename);
+void parse_dynamic_uri(char *uri,char *filename,char *cgiargs);
+void feed_static(int fd,char *filename,int filesize);
+void get_filetype(char *filename,char *filetype);
+void feed_dynamic(int fd,char *filename,char *cgiargs);
+void error_request(int fd,char *cause,char *errnum,char *shortmsg,char *desprition);
+
+int main()
+{
+	
+}
