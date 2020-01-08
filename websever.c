85d40f201234c0befb652807d1512bb106caa266
Zzk995 <983558459@qq.com>
2020-01-08 10:47:13 +0800

    add function named process_trans

--- websever.c
+++ websever.c
@@ -29,3 +29,49 @@ int main(int argc,char **argv)
 		close(conn_sock);
 	}
 }
+
+void process_trans(int fd)
+{
+	int static_flag;
+	struct stat sbuf;
+	char buf[MAXLINE],method[MAXLINE],uri[MAXLINE],version[MAXLINE];
+	char filename[MAXLINE],cgiargs[MAXLINE];
+	rio_t rio;
+
+	rio_readinitb(&rio,fd);
+	rio_readlineb(&rio,buf,MAXLINE);
+	sscanf(buf,"%s %s %s",method,uri,version);
+	if(strcasecmp(method,"GET")){
+		error_request(fd,method,"501","Not Implemented","web does not implement this method");
+		return;
+	}
+	read_requeshdrs(&rio);
+
+	static_flag=is_static(uri);
+	if(static_flag)
+		parse_static_uri(uri,filename);
+	else
+		parse_dynamic_uri(uri,filename,cgisrgs);
+
+	if(stat(filename,&sbuf)<0){
+		error_request(fd,filename,"404","Not Found","web could not find this find");
+		return;
+	}
+
+	if(static_flag){
+		if(!(S_ISREG(sbuf.st_mode))||!(S_IRUSR & sbuf.st_mode)){
+			error_request(fd,filename,"403","Forbidden","web is not permtted to read the file");
+			return;
+		}
+		feed_static(fd,filename,sbuf.st_size);
+	}else{
+		if(!(S_ISREG(sbuf.st_mode))||!(S_IXUSR &sbuf.st_mode)){
+			error_request(fd,filename,"403","Forbidden","web could not run the CGI program");
+			return;
+		}
+		feed_static(fd,filename,cgiargs);
+	}
+}
+
+
+
