bbe96df4eb186bd5d72f21ecceaa8c8177445564
Zzk995 <983558459@qq.com>
2020-01-08 09:24:21 +0800

    add some variable definitions

--- websever.c
+++ websever.c
@@ -10,7 +10,22 @@ void get_filetype(char *filename,char *filetype);
 void feed_dynamic(int fd,char *filename,char *cgiargs);
 void error_request(int fd,char *cause,char *errnum,char *shortmsg,char *desprition);
 
-int main()
+int main(int argc,char **argv)
 {
-	
+	int listen_sock,conn_sock,port,clientlen;
+	struct sockaddr_in clientaddr;
+
+	if(argc!=2){
+		fprintf(stderr,"usage:%s<port>\n",argc[0]);
+		exit(0);
+	}
+	port = atoi(argc[1]);
+
+	listen_sock=open_listen_sock(port);
+	while(1){
+		clientlen=sizeof(clientaddr);
+		conn_sock=accpt(listen_sock,(SA *)&clientaddr,&clientlen);
+		process_trans(conn_sock);
+		close(conn_sock);
+	}
 }
