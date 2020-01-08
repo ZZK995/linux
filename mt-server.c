17bf7944c34d910a7a58ad19c5429a769770d7de
senlin-w <2352343270@qq.com>
2020-01-08 19:14:47 +0800

    create mt-server.c  for multithreading file

---
+++ mt-server.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include "lx_mt_server.h"
+#include "lxtime.h"
+#include "lx_http.h"
+#include "lx_http_util.h"
+#include "lxlog.h"
+
+#define g_ctx (g_lxmt_server_ctx)
+
+struct lxmt_hdarg
+{
+    lx_connection conn;
+};
+typedef struct lxmt_hdarg lxmt_hdarg; 
+
+struct lxmt_server_ctx 
+{
+    int stimeo_milli;
+    int rtimeo_milli;
+    lx_bool_t is_nostub;
+    struct lxlog log;
+    struct lxlog_dailyas asarg;
+};
+typedef struct lxmt_server_ctx lxmt_server_ctx;
+
+static lxmt_server_ctx *g_lxmt_server_ctx;
+
+static char * g_home = "home";
+static char * g_whome = "webhome";
+static char * g_loghome = "logs";
+
+int init_lxmt_server(lx_bool_t is_nostub,char * phome)
+{
+    char buff[1024];
+    int ret = 0;
+    struct lxmt_server_ctx * ctx;
+    ctx = (lxmt_server_ctx *)malloc(sizeof(lxmt_server_ctx));
+    if( ctx == NULL)
+    {
+        perror("malloc in init error\n");
+        return -1;
+    }
+    ctx->stimeo_milli = 200000;
+    ctx->rtimeo_milli = 200000;
+    ctx->is_nostub = is_nostub;
+   
+    if(phome)
+        g_home = phome;
+  
+    newlxlog( (&ctx->log));
+    ctx->asarg.newhour = 18;
+    ctx->log.arg = &ctx->asarg;
+    if(snprintf(buff,1024,"%s/%s",g_home,g_loghome) <=0 || 
+        (ret = lxlog_init(&ctx->log,buff,"access.log", LX_LOG_DEBUG)) ){
+        printf("init log error,ret=%d",ret);
+        return EXIT_FAILURE;
+    }
+    ctx->log.flushnow = 1;
+    ctx->log.tlockflag = 1;
+    ctx->log.plockflag = 0;
+    ctx->log.showpid = 1;
+   // ctx->log.showtid = 0;
+
+    g_lxmt_server_ctx = ctx;
+
+    return 0;
+}
+
