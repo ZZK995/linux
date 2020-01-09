f4f6ea5e00ec2dff318dc545aa79c765e9e42cd3
ZZK995 <983558459@qq.com>
2020-01-09 15:57:42 +0800

    modified html & mult

--- add.c
+++ add.c
@@ -3,19 +3,16 @@
 int main(void) {
     char *buf, *p;
     char content[MAXLINE];
-    int n1=0, n2=0;
+    int m=5, n=5;
 
-    /* Extract the two arguments from standard input */
-    scanf("%d&%d", &n1, &n2);
+    scanf("%d&%d", &m, &n);
     
-    /* Make the response body */
-    sprintf(content, "Welcome to add.com: ");
+    sprintf(content, "Multiplication: ");
     sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
-    sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", 
-	    content, n1, n2, n1 + n2);
-    sprintf(content, "%sThanks for visiting!\r\n", content);
+    sprintf(content, "%sThe answer is: %d * %d = %d\r\n<p>", 
+	    content, m, n, m * n);
+    sprintf(content, "%sThanks!\r\n", content);
 
-    /* Generate the HTTP response */
     printf("Content-length: %d\r\n", strlen(content));
     printf("Content-type: text/html\r\n\r\n");
     printf("%s", content);
